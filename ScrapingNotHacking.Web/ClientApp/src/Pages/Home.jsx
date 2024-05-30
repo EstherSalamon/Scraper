f57794e192a424186ade7b7162e56289d4154f66
Esther Salamon <esther386386@gmail.com>
2024-05-30 16:59:39 -0400

    This stuff is awesomely awesome!

--- ScrapingNotHacking.Web/ClientApp/src/Pages/Home.jsx
+++ ScrapingNotHacking.Web/ClientApp/src/Pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
             <div className="d-flex flex-column justify-content-center align-items-center" style={{ marginTop: 11000 }} >
                 <h1>These are scraped articles</h1>
                 {articles.map(item =>
-                    <span key={item.title}>
+                    <span key={item.title}>  
                         <div className="card" style={{ width: 500 }} >
                             <div className="card-body">
                                 <h5 className="card-title">{item.title}</h5>
